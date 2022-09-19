43e363acdd572719cfc781d195419c51af734732
Manav Misra <manavm@visionwebsoft.com>
2022-09-18 21:44:28 -0500

    feat: :white_check_mark: array rotations

--- app/index.js
+++ app/index.js
@@ -3,3 +3,12 @@ export const getBinaryGap = (number) => {
   const gaps = binary.split("1").filter((zeroGaps) => zeroGaps.length > 0);
   return gaps.length > 0 ? Math.max(...gaps.map((gap) => gap.length)) : 0;
 };
+
+export const rotate = (arr, rotations) => {
+  const rotated = [...arr];
+  for (let i = 0; i < rotations; i++) {
+    rotated.unshift(rotated.pop());
+  }
+
+  return rotated;
+};
--- app/index.test.js
+++ app/index.test.js
@@ -1,6 +1,12 @@
 import { expect, test } from "vitest";
-import { getBinaryGap } from "./index.js";
+import { getBinaryGap, rotate } from "./index.js";
 
 test("binary gap of 1041 is 5", () => {
   expect(getBinaryGap(1041)).toBe(5);
+});
+
+test("arrays are rotated", () => {
+  expect(rotate([3, 8, 9, 7, 6], 3)).toEqual([9, 7, 6, 3, 8]);
+  expect(rotate([0, 0, 0], 1)).toEqual([0, 0, 0]);
+  expect(rotate([1, 2, 3, 4], 4)).toEqual([1, 2, 3, 4]);
 });
